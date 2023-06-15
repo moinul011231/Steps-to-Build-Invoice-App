baf6ef4cc239176f580c9523bc04a3a8097aa43a
poly_wahida <ifrahpoly@gmail.com>
2023-06-15 23:44:36 +0600

    Ask users to select their products by writing the product names as shown in the display.

--- InvoiceApp1/InvoiceApp1/Program.cs
+++ InvoiceApp1/InvoiceApp1/Program.cs
@@ -16,12 +16,12 @@ namespace InvoiceApp1
             Invoiceapp.PrintTitle("Available Products");
             var products = new[]
            {
-                new { Name = "Product 1", Price = 10 },
-                new { Name = "Product 2", Price = 20 },
-                new { Name = "Product 3", Price = 30 },
-                new { Name = "Product 4", Price = 40 },
-                new { Name = "Product 5", Price = 50 },
-                new { Name = "Product 6", Price = 60 },
+                new { Name = "Product 1", Price = 15 },
+                new { Name = "Product 2", Price = 15 },
+                new { Name = "Product 3", Price = 20 },
+                new { Name = "Product 4", Price = 25 },
+                new { Name = "Product 5", Price = 30 },
+                new { Name = "Product 6", Price = 35 },
             };
 
             foreach (var product in products)
@@ -29,7 +29,15 @@ namespace InvoiceApp1
                 Console.WriteLine("{0} ({1})", product.Name, product.Price);
             }
             Console.WriteLine();
+            var productNames = new List<string>();
+            var quantities = new List<int>();
 
+            while (true)
+            {
+                Console.WriteLine("Enter the product name: ");
+                var productName = Console.ReadLine();
+            }
         }
-    }
+
+    }   
 }
