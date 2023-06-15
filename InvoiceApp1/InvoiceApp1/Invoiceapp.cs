b9c056deca40a1ba31d4de48016a9f311b654aef
poly_wahida <ifrahpoly@gmail.com>
2023-06-15 21:34:06 +0600

    Title and Welcome Message code.

---
+++ InvoiceApp1/InvoiceApp1/Invoiceapp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp1
+{
+    internal class Invoiceapp
+    {
+        public static void PrintWelcomeMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Welcome to TechByte!");
+            Console.ResetColor();
+        }
+        public static void PrintTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"===== {title} =====");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ InvoiceApp1/InvoiceApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoiceapp.PrintTitle("TechByte");
+            Invoiceapp.PrintWelcomeMessage();
+            
+        }
+    }
+}
